714014404cdb2200d5db05e50174650a1441eb02
brijq <brijq91@gmail.com>
2017-04-02 23:00:17 +0800

    configurations

---
+++ web-start/firebase-messaging-sw.js
@@ -0,0 +1,36 @@
+importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  messagingSenderId: '670781077200'
+});
+var messaging = firebase.messaging();
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// [START background_handler]
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
+// [END background_handler]
+
+// Handle incoming messages. Called when:
+// - a message is received while the app has focus
+// - the user clicks on an app notification created by a sevice worker
+//   `messaging.setBackgroundMessageHandler` handler.
+messaging.onMessage(function(payload) {
+  console.log("Message received. ", payload);
+  // ...
+});
